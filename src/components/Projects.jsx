ac6068a9358b39c525fb1b2c132ee7b7e8621164
Nam Doan <southeast.samstation11@gmail.com>
2025-06-17 03:25:31 +0200

    fix: image Datei Name korrigiert für Netlify

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import WetterKit from "../assets/Images/WetterKit.png";
-import OptiCutImg from "../assets/Images/Opticut.png";
+import OptiCutImg from "../assets/Images/OptiCut.png";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
